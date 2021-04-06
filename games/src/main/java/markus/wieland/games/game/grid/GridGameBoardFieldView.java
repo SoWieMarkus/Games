a2c5e8451ab993543b5de08cd916772e73c71422
= <=>
2021-04-06 19:48:03 +0200

    changed getX to getCoordinate

--- games/src/main/java/markus/wieland/games/game/grid/GridGameBoardFieldView.java
+++ games/src/main/java/markus/wieland/games/game/grid/GridGameBoardFieldView.java
@@ -1,11 +1,10 @@
 package markus.wieland.games.game.grid;
 
+import markus.wieland.games.elements.Coordinate;
 import markus.wieland.games.game.view.GameBoardFieldView;
 
 public interface GridGameBoardFieldView extends GameBoardFieldView {
 
-    int getX();
-
-    int getY();
+    Coordinate getCoordinate();
 
 }
