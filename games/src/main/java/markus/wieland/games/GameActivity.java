12dafb2f357180fda30226743b769a3dc0c95164
SoWieMarkus <wie.markus@web.de>
2021-04-08 15:23:14 +0200

    fixed delete game too early bug

--- games/src/main/java/markus/wieland/games/GameActivity.java
+++ games/src/main/java/markus/wieland/games/GameActivity.java
@@ -108,11 +108,17 @@ public abstract class GameActivity<C extends GameConfiguration, H extends Highsc
     @Override
     public void onAbort(ConfirmDialog confirmDialog) {
         if (confirmDialog != null) {
-            if (game != null) game.setRunning(false);
-            if (gameSaver != null) gameSaver.delete();
-            confirmDialog.setConfirmClickListener((dialog, which) -> restartActivity(false));
+            confirmDialog.setConfirmClickListener((dialog, which) -> {
+                abortGame();
+            });
             confirmDialog.getDialog(this).show();
         }
+    }
+
+    protected void abortGame(){
+        if (game != null) game.setRunning(false);
+        if (gameSaver != null) gameSaver.delete();
+        restartActivity(false);
     }
 
     protected abstract GameGenerator<S> initializeGenerator(GameConfiguration configuration);
